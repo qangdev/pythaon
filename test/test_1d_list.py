22d928d6b581d22f0cca18d31c501d84d8dc3ccc
Thao Vo <thaovo@schwabe.net>
2021-06-29 20:43:59 +0700

    fix the Index error message

--- test/test_1d_list.py
+++ test/test_1d_list.py
@@ -2,15 +2,17 @@ from typing import List
 
 my_list = [1, 2, 3, 4, 5]
 
-
+# This function should return all values at the given index
+# https://stackoverflow.com/questions/54981448/how-to-avoid-indexerror-list-index-out-of-range-error
 def get_values_at_index(index: int, data: list):
-    # This function should return all values at the given index
-    return data[index]
-
+    try:
+        return data[index]
+    except IndexError:
+        return None
 
 
 
 def test_get_values_at_index_returns_a_value():
     assert get_values_at_index(0, my_list) == 1
     assert get_values_at_index(2, my_list) == 3
-    # assert get_values_at_index(6, my_list) is None   'note: IndexError: list index out of range'
+    assert get_values_at_index(6, my_list) is None
--- test/test_2d_list.py
+++ test/test_2d_list.py
@@ -5,28 +5,34 @@ list_2 = [6, 7, 8, 9, 10]
 list_in_list = [list_1, list_2]
 
 
-
+# This function should return all values at the given column index
 def get_values_at_column(col_index: int, data: List[list]):
-    # This function should return all values at the given column index
     result = []
-    for i in range(len(data)):
-        a = data[i][col_index]
-        result.append(a)
-    return result
+    try:
+        for i in range(len(data)):
+            a = data[i][col_index]
+            result.append(a)
+        return result
+    except IndexError:
+        return None
 
 
 def get_cell_value_at(x: int, y: int, data: List[list]):
     # This function should return a values at the given row index (x), and column index (y)
-    return data[x][y]
+    try:
+        return data[x][y]
+    except IndexError:
+        return None
 
 
 def test_get_values_at_column_returns_a_list_of_value():
     assert get_values_at_column(0, list_in_list) == [1, 6]
     assert get_values_at_column(2, list_in_list) == [3, 8]
-    # assert get_values_at_column(13, list_in_list) is None  list index out of range (make unittest fail)
+    assert get_values_at_column(13, list_in_list) is None
 
 
 def test_get_cell_value_at_returns_a_single_value():
     assert get_cell_value_at(0, 1, list_in_list) == 2
     assert get_cell_value_at(1, 0, list_in_list) == 6
-#     assert get_cell_value_at(100, 100, list_in_list) is None   list index out of range (make unit-test fail)
+    assert get_cell_value_at(100, 100, list_in_list) is None
+
